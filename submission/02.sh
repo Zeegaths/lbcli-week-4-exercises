976e73580d5b3ec262dce9f9be71b72fccebe9fe
Zeegaths <your.email@example.com>
2025-04-07 10:25:27 +0300

    submission

--- submission/02.sh
+++ submission/02.sh
@@ -9,7 +9,7 @@
 transaction="01000000000101c8b0928edebbec5e698d5f86d0474595d9f6a5b2e4e3772cd9d1005f23bdef772500000000ffffffff0276b4fa0000000000160014f848fe5267491a8a5d32423de4b0a24d1065c6030e9c6e000000000016001434d14a23d2ba08d3e3edee9172f0c97f046266fb0247304402205fee57960883f6d69acf283192785f1147a3e11b97cf01a210cf7e9916500c040220483de1c51af5027440565caead6c1064bac92cb477b536e060f004c733c45128012102d12b6b907c5a1ef025d0924a29e354f6d7b1b11b5a7ddff94710d6f0042f3da800000000"
 
 # Decode the transaction to JSON for parsing
-decoded_tx=$(bitcoin-cli decoderawtransaction "$transaction")
+decoded_tx=$(bitcoin-cli -regtest decoderawtransaction "$transaction")
 
 # Extract the TXID of this transaction (this will be our input UTXO's txid)
 txid=$(echo "$decoded_tx" | jq -r '.txid')
@@ -31,13 +31,13 @@ echo "TXID: $txid"
 echo "Vout: $vout"
 
 # Create the raw transaction with locktime
-rawtxhex=$(bitcoin-cli -named createrawtransaction \
+rawtxhex=$(bitcoin-cli -regtest -named createrawtransaction \
   inputs='''[ { "txid": "'$txid'", "vout": '$vout' } ]''' \
   outputs='''{ "'$recipient'": '$amount' }''' \
   locktime=$locktime)
 
 # Sign the transaction (this doesn't broadcast it)
-signedtx=$(bitcoin-cli -named signrawtransactionwithwallet hexstring=$rawtxhex | jq -r '.hex')
+signedtx=$(bitcoin-cli -regtest -named signrawtransactionwithwallet hexstring=$rawtxhex | jq -r '.hex')
 
 # Print information about the transaction
 echo "Created time-locked transaction:"
