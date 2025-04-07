9880818cd294402fc8a05bd92bf8f62dde0c0cdc
Zeegaths <your.email@example.com>
2025-04-07 10:21:32 +0300

    submission

--- submission/02.sh
+++ submission/02.sh
@@ -3,3 +3,54 @@
 # Amount of 20,000,000 satoshis to this address: 2MvLcssW49n9atmksjwg2ZCMsEMsoj3pzUP 
 # Use the UTXOs from the transaction below
 # transaction="01000000000101c8b0928edebbec5e698d5f86d0474595d9f6a5b2e4e3772cd9d1005f23bdef772500000000ffffffff0276b4fa0000000000160014f848fe5267491a8a5d32423de4b0a24d1065c6030e9c6e000000000016001434d14a23d2ba08d3e3edee9172f0c97f046266fb0247304402205fee57960883f6d69acf283192785f1147a3e11b97cf01a210cf7e9916500c040220483de1c51af5027440565caead6c1064bac92cb477b536e060f004c733c45128012102d12b6b907c5a1ef025d0924a29e354f6d7b1b11b5a7ddff94710d6f0042f3da800000000"
+#!/bin/bash
+
+# The raw transaction to analyze
+transaction="01000000000101c8b0928edebbec5e698d5f86d0474595d9f6a5b2e4e3772cd9d1005f23bdef772500000000ffffffff0276b4fa0000000000160014f848fe5267491a8a5d32423de4b0a24d1065c6030e9c6e000000000016001434d14a23d2ba08d3e3edee9172f0c97f046266fb0247304402205fee57960883f6d69acf283192785f1147a3e11b97cf01a210cf7e9916500c040220483de1c51af5027440565caead6c1064bac92cb477b536e060f004c733c45128012102d12b6b907c5a1ef025d0924a29e354f6d7b1b11b5a7ddff94710d6f0042f3da800000000"
+
+# Decode the transaction to JSON for parsing
+decoded_tx=$(bitcoin-cli decoderawtransaction "$transaction")
+
+# Extract the TXID of this transaction (this will be our input UTXO's txid)
+txid=$(echo "$decoded_tx" | jq -r '.txid')
+
+# Extract the vout information
+# Typically, we'd choose one of the vouts as our input
+# For this example, we'll use the first output (vout 0)
+vout=0
+
+# Set up the transaction parameters
+recipient="2MvLcssW49n9atmksjwg2ZCMsEMsoj3pzUP"
+amount=0.2
+current_block=25
+blocks_in_two_weeks=2016
+locktime=$((current_block + blocks_in_two_weeks))
+
+echo "Extracted UTXO information from the transaction:"
+echo "TXID: $txid"
+echo "Vout: $vout"
+
+# Create the raw transaction with locktime
+rawtxhex=$(bitcoin-cli -named createrawtransaction \
+  inputs='''[ { "txid": "'$txid'", "vout": '$vout' } ]''' \
+  outputs='''{ "'$recipient'": '$amount' }''' \
+  locktime=$locktime)
+
+# Sign the transaction (this doesn't broadcast it)
+signedtx=$(bitcoin-cli -named signrawtransactionwithwallet hexstring=$rawtxhex | jq -r '.hex')
+
+# Print information about the transaction
+echo "Created time-locked transaction:"
+echo "--------------------------------"
+echo "UTXO: $txid:$vout"
+echo "Recipient: $recipient"
+echo "Amount: $amount BTC (20,000,000 satoshis)"
+echo "Locktime: Block $locktime (2 weeks from current block $current_block)"
+echo
+echo "Raw transaction hex:"
+echo $rawtxhex
+echo
+echo "Signed transaction hex:"
+echo $signedtx
+echo
+echo "This transaction cannot be mined until block $locktime is reached."
