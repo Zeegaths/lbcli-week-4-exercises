145614e4dc932bb64e6a540ab42a8aa22674c817
Zeegaths <your.email@example.com>
2025-04-07 14:19:50 +0300

    submission

--- submission/03.sh
+++ submission/03.sh
@@ -5,20 +5,11 @@
 # transaction="01000000000101c8b0928edebbec5e698d5f86d0474595d9f6a5b2e4e3772cd9d1005f23bdef772500000000ffffffff0276b4fa0000000000160014f848fe5267491a8a5d32423de4b0a24d1065c6030e9c6e000000000016001434d14a23d2ba08d3e3edee9172f0c97f046266fb0247304402205fee57960883f6d69acf283192785f1147a3e11b97cf01a210cf7e9916500c040220483de1c51af5027440565caead6c1064bac92cb477b536e060f004c733c45128012102d12b6b907c5a1ef025d0924a29e354f6d7b1b11b5a7ddff94710d6f0042f3da800000000"
 #!/bin/bash
 
-transaction="01000000000101c8b0928edebbec5e698d5f86d0474595d9f6a5b2e4e3772cd9d1005f23bdef772500000000ffffffff0276b4fa0000000000160014f848fe5267491a8a5d32423de4b0a24d1065c6030e9c6e000000000016001434d14a23d2ba08d3e3edee9172f0c97f046266fb0247304402205fee57960883f6d69acf283192785f1147a3e11b97cf01a210cf7e9916500c040220483de1c51af5027440565caead6c1064bac92cb477b536e060f004c733c45128012102d12b6b907c5a1ef025d0924a29e354f6d7b1b11b5a7ddff94710d6f0042f3da800000000"
-
-# Get the txid of this transaction
-txid=$(bitcoin-cli -regtest decoderawtransaction $transaction | jq -r .txid)
-
-# Convert our message to hex
-message="btrust builder 2025"
-message_hex=$(echo -n "$message" | xxd -p -u)
-
-# The recipient address and amount
+txid="23c19f37d4e92e9a115aab86e4edc1b92a51add4e0ed00b36b3146d15dee0e16"
 recipient="2MvLcssW49n9atmksjwg2ZCMsEMsoj3pzUP"
-amount=0.2
+message="btrust builder 2025"
 
-# Create raw transaction with OP_RETURN data
+# Create raw transaction with OP_RETURN data first, then monetary output
 bitcoin-cli -regtest -named createrawtransaction \
   inputs='''[
     {
@@ -34,10 +25,10 @@ bitcoin-cli -regtest -named createrawtransaction \
   ]''' \
   outputs='''[
     {
-      "'$recipient'": '$amount'
+      "data": "'$(echo -n $message | xxd -p -u)'"
     },
     {
-      "data": "'$message_hex'"
+      "'$recipient'": 0.2
     }
   ]''' \
-  locktime=2041
+  locktime=0
